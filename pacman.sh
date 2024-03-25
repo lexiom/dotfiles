d129fd5a0725516e7cc6a7e075628d3dbb804168
lexiom <datalexdev@gmail.com>
2024-03-25 15:38:57 -0300

    fixed waybar calendar display

---
+++ pacman.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+sudo pacman -S \
+  alacritty \
+  dunst \
+  fish \
+  git \
+  gnome-calendar \
+  grim \
+  hyprland \
+  hyprpaper \
+  inter-font \
+  keepassxc \
+  nautilus \
+  noto-fonts-emoji \
+  nwg-look \
+  openssh \
+  podman \
+  podman-docker \
+  polkit-kde-agent \
+  slurp \
+  starship \
+  ttf-dejavu \
+  ttf-jetbrains-mono-nerd \
+  ttf-nerd-fonts-symbols \
+  unzip \
+  vim \
+  waybar \
+  wl-clipboard \
+  xdg-desktop-portal-gtk \
+  xdg-desktop-portal-hyprland
---
+++ paru.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+
+git clone https://aur.archlinux.org/paru.git \
+  && cd paru \
+  && makepkg -si
